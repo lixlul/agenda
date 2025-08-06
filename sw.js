2d7299c666e67bf1b60c1bd93d2be7537548c138
Ana Luiza <demoraesraimundoa@gmail.com>
2025-08-06 10:47:35 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'pwa-task-list-v1';
+const urlsToCache = [
+    '/agenda/sw.js',
+    '/agenda/index.html',
+    '/agenda/style.css',
+    '/agenda/manifest.json',
+    '/agenda/icons/icon-192x192.png',
+    '/agenda/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
